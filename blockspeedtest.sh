5be8615cbaae61bee7a8835866d21e1211919fb1
Quoctai020901 <99971842+Quoctai020901@users.noreply.github.com>
2022-09-24 23:58:59 +0700

    Update blockspeedtest.sh

--- blockspeedtest.sh
+++ blockspeedtest.sh
@@ -7,7 +7,7 @@ read -p "  Vui Lòng Nhập : " num
     case "${num}" in
         1) apt-get update && apt install iptables-persistent netfilter-persistent -y && clear && echo "   DONE !"
         ;;
-        2) bash <(curl -Ls https://raw.githubusercontent.com/quoctai020901/script/main/speedtest/blockip.sh)
+        2) bash <(curl -Ls https://raw.githubusercontent.com/quoctai020901/speedtest/main/blockip.sh)
         ;;
         3) iptables -F && clear && echo "   DONE !"
         ;;
